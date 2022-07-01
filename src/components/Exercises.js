fb2314abc7caa52f38a10ec2843dbfde71104487
xcixor <pndungu54@gmail.com>
2022-07-02 02:30:33 +0300

    chore(pagination): implement exercises pagination

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { Pagination } from '@mui/material/Pagination';
+import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
@@ -8,6 +8,18 @@ import ExerciseCard from './ExerciseCard';
 
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise)
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: 'smooth' });
+  }
+
   return (
     <Box
       id="exercises"
@@ -16,8 +28,11 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       p="20px"
     >
       <Typography
-        variant="h3"
+        variant="h4"
         mb="46px"
+        fontWeight="bold"
+        sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+        ml="40px"
       >Showing Results</Typography>
       <Stack
         direction="row"
@@ -25,9 +40,24 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         flexWrap="wrap"
         justifyContent="center"
       >
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+          {exercises.length > 9 && (
+            <Pagination
+              color="standard"
+              shape="rounded"
+              defaultPage={1}
+              count={Math.ceil(exercises.length / exercisesPerPage)}
+              page={currentPage}
+              onChange={paginate}
+              size="large"
+            />
+          )
+
+          }
       </Stack>
     </Box>
   )
